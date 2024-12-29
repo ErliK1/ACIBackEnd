4d0660551da5fcf4f3824a5925a0fc852a83c017
ErliK1 <ekuka21@epoka.edu.al>
2024-12-29 14:02:57 +0100

    Fixed sort and order

--- product/views.py
+++ product/views.py
@@ -18,19 +18,19 @@ from functools import reduce
 class ProductListAPIView(ACIListAPIView):
     serializer_class = ProductListSerializer
     filter_map = {
-            'category': 'product_categories__category__id',
-            'brand': 'brand__id',
-            'name': 'name',
-            'discount': 'has_discount',
+            'category': 'product_categories__category__id', # list of ids (numbers)
+            'brand': 'brand__id', #id (number)
+            'name': 'name', # string 
+            'discount': 'has_discount', # boolean
         }
     sort_map = {
-        'popularity': 'product_popularities__count',
+        'popularity': 'product_popularities__product_count',
         'name': 'name',
         }
     filter_serializer_class = ProductListFilterSerializer
 
 
-    def get_queryset(self, ):     
+    def get_queryset(self, ):
         popularity_queryset = Product.objects.filter(deleted=False)
         if self.request.query_params.get('lt_price') and self.request.query_params.get('lt_price').isnumeric():
             price = float(self.request.query_params.get('lt_price'))
