a2f49060ea7fb813b28ada066b6508b56bbd48c9
Alvi Dervishaj <alvidervishaj9@gmail.com>
2025-01-12 14:32:08 +0100

    Fixed the pk value on update url at /product/update. It now accepts an it like: /product/update/<ID>.

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.urls import include
-
+from rest_framework.urlpatterns import format_suffix_patterns
 from product.model_views.order_views import OrderCreateAPIView, OrderListFromManagerAPIView
 from product.model_views.product_views import ProductCreateAPIView, CategoryCreateListAPIView, BrandCreateListAPIView, ProductUpdateAPIView, ProductInsertDiscountView
 from product.url_constans import *
@@ -9,11 +9,11 @@ from product.views import ProductListAPIView
 urlpatterns = [
     path('list/', ProductListAPIView.as_view(), name=PRODUCT_LIST),
     path('create/', ProductCreateAPIView.as_view(), name=PRODUCT_CREATE),
-    path('update/', ProductUpdateAPIView.as_view(), name=PRODUCT_UPDATE),
+    path('update/<int:pk>/', ProductUpdateAPIView.as_view(), name=PRODUCT_UPDATE),
     path('category/list/create/', CategoryCreateListAPIView.as_view(), name=CATEGORY_LIST_CREATE),
     path('brand/list/create/', BrandCreateListAPIView.as_view(), name=BRAND_LIST_CREATE),
     path('order/create/', OrderCreateAPIView.as_view(), name=ORDER_CREATE),
     path('order/list/', OrderListFromManagerAPIView.as_view(), name=ORDER_LIST),
     path('discount/set/', ProductInsertDiscountView.as_view())
-    
-] 
+]
+urlpatterns = format_suffix_patterns(urlpatterns)
